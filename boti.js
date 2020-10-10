7bcf6a508f853976290b6f87fa4bb9516cb27ccf
Giselle Rosa <gisellenrosa@gmail.com>
2020-10-10 13:31:04 -0300

    First commit - Aulas iniciais -- HTML - CSS

---
+++ boti.js
@@ -0,0 +1,10 @@
+const Tampas = 30 
+
+For ( var i = 1; i <= 10 ; i++)
+
+
+if (Tampas > 50) {"tampas >50"} 
+else if (Tampas < 10) {"Tampas <10"}
+else if (Tampas > 100) { "Tampas >100} 
+else if (Tampas > 10, Tampas < 50) {"Tampas entre 10 e 50"} 
+else {"Não suportado"}
